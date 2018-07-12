787728967a6b3910e1a892d07a0b09ec9bab000d
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-12 20:29:39 +0300

    Add function to show that user not found

--- js/ui.js
+++ js/ui.js
@@ -31,6 +31,29 @@ class UI {
     `;
   }
 
+  showAlert(message, className) {
+    this.clearAlert();
+    const div = document.createElement('div');
+
+    div.className = className;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.searchContainer'),
+          search = document.querySelector('.search');
+    
+    container.insertBefore(div, search);
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
   clearProfile() {
     this.profile.innerHTML = '';
   }
