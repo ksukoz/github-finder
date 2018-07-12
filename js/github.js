f81a29c7901445e5172663d5adbc3511de99cee1
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-12 22:42:24 +0300

    Add user`s repos

--- js/github.js
+++ js/github.js
@@ -2,15 +2,23 @@ class GitHub {
   constructor() {
     this.client_id = '65e20bc6f13644092751';
     this.client_secret = '7d0e2ea54e61935cec799ea7546f05968223e273';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
+
+  
 }
